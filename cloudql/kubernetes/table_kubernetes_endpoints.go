88619449e71a46e5d7eec0b994414631161e42d0
Mahan Zendedel DH <mahanmmi@gmail.com>
2025-02-13 16:19:27 +0400

    fix: cloudql table names

--- cloudql/kubernetes/table_kubernetes_endpoints.go
+++ cloudql/kubernetes/table_kubernetes_endpoints.go
@@ -10,7 +10,7 @@ import (
 
 func tableKubernetesEndpoints(ctx context.Context) *plugin.Table {
 	return &plugin.Table{
-		Name:        "kubernetes_endpoint",
+		Name:        "kubernetes_endpoints",
 		Description: "Set of addresses and ports that comprise a service. More info: https://kubernetes.io/docs/concepts/services-networking/service/#services-without-selectors.",
 		Get: &plugin.GetConfig{
 			Hydrate: opengovernance.GetKubernetesEndpoint,
